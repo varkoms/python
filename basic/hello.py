b7c9dd3cfb41aafbf78162fc6307c67deb283030
varkoms <cmendozasoberanis@gmail.com>
2023-02-16 16:49:16 -0600

    Hello Python

---
+++ basic/hello.py
@@ -0,0 +1,28 @@
+# Esto es un comentario
+# Nuestro primer hola mundo en Python
+
+print("Hola Python")
+print('Hola Python')
+
+"""
+Esto es un
+comentario
+en varias lineas
+
+Esto es ignorado y no se imprime
+en pantalla
+"""
+
+'''
+Este tambien es un comentario
+en varias lineas. Al igual que
+con las comillas dobles,
+esto sera ignorado
+'''
+
+# Consultar el tipo de
+print(type("Hola Python")) # <class 'str'>
+print(type(5)) # <class 'int'>
+print(type(1.5)) # <class 'float'>
+print(type(3 + 1j)) # <class 'complex'>
+print(type(True)) # <class 'bool'>
