ebfe69486522c4fbffc6273a13387c410e227cd9
Cesar Mendoza <cmendozasoberanis@gmail.com>
2023-03-13 09:12:00 -0600

    Edited on nvim

--- basic/11_classes.py
+++ basic/11_classes.py
@@ -2,8 +2,8 @@
 
 class Person:
     def __init__(self, name, surname, alias="Sin Alias"):
-        self.__name = name # Propiedad privada
-        self.__surname = surname # Propiedad privada
+        self.__name = name  # Propiedad privada
+        self.__surname = surname  # Propiedad privada
         self.full_name = f"{name} {surname} ({alias})"
         self.alias = alias
 
@@ -14,9 +14,10 @@ class Person:
         print(f"{self.alias} esta caminando...")
 
 my_person = Person("Cesar", "Mendoza", "Varko")
-print(my_person.full_name) # Cesar Mendoza
+
+print(my_person.full_name)  # Cesar Mendoza
 my_person.full_name = "Hector de Leon (El loco de los perros)"
-print(my_person.full_name) # Hector de Leon (El loco de los perros)s
+print(my_person.full_name)  # Hector de Leon (El loco de los perros)s
 # print(my_person.alias) # Varko
-my_person.walk() # Varko esta caminando
+my_person.walk()  # Varko esta caminando
 print(my_person.get_name()) # Cesar
