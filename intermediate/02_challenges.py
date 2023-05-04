9f667143e6d6ed078c800bce227ec7010ba92519
Cesar Mendoza <cmendozasoberanis@gmail.com>
2023-05-04 13:48:05 -0600

    is_prime() challenge

--- intermediate/02_challenges.py
+++ intermediate/02_challenges.py
@@ -66,4 +66,30 @@ def fibonacci():
         next = fib
 
 
-fibonacci()
+# fibonacci()
+
+"""
+¿ES UN NUMERO PRIMO?
+Escribe un programa que se encargue de comprobar si un número es o no primo.
+Hecho esto, imrpime los números primos entre 1 y 100
+"""
+
+
+def is_prime():
+
+    for number in range(1, 101):
+
+        if number >= 2:
+
+            is_divisible = False
+
+            for index in range(2, number):
+                if number % index == 0:
+                    is_divisible = True
+                    break
+
+                if not is_divisible:
+                    print(number)
+
+
+is_prime()
