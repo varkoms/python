c9e78ed32b9e800fa223aa0af73b899f67a4c5de
Cesar Mendoza <cmendozasoberanis@gmail.com>
2023-05-04 15:10:41 -0600

    invirtiendo cadenas a mano

--- intermediate/02_challenges.py
+++ intermediate/02_challenges.py
@@ -92,4 +92,22 @@ def is_prime():
                     print(number)
 
 
-is_prime()
+# is_prime()
+
+"""
+INVIRTIENDO CADENAS A MANO
+Crea un programa que invierta el orden de una cadenad de texto
+SIN USAR funciones propias del lenguaje que lo hagan de forma automática.
+- Si le pasamos "Hola mundo", el programa nos retornaría "odnum aloH"
+"""
+
+
+def reverse(text):
+    text_len = len(text)
+    reversed_text = ""
+    for index in range(0, text_len):
+        reversed_text += text[text_len - index - 1]
+    return reversed_text
+
+
+print(reverse("Hola Mundo"))
