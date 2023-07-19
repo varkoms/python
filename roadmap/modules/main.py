ce24dfd3635a59cbde99e8fd355d6852be85aa16
Cesar Mendoza <cmendozasoberanis@gmail.com>
2023-07-19 15:05:31 -0600

    Importing and renaming modules in Python

--- roadmap/modules/main.py
+++ roadmap/modules/main.py
@@ -9,3 +9,16 @@ When the interpreter encounters an import statement, it imports the module if th
 import calc
 
 print(calc.add(10, 2)) # 12
+
+# Renaming the Python module
+# We can rename the module while importing it using the keyword.
+
+# importing sqrt() and factorial from the
+# module math
+import math as mt
+
+# if we simply do "import math", then
+# math.sqrt(16) and math.factorial()
+# are required.
+print(mt.sqrt(16)) # 4.0
+print(mt.factorial(6)) # 720
