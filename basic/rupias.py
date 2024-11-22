db84442405c356b02ba41a0a178c5cbb87d8cf30
varkoms <cmendozasoberanis@gmail.com>
2024-11-22 12:36:32 -0600

    Updated rupias.py

--- basic/rupias.py
+++ basic/rupias.py
@@ -2,15 +2,12 @@ import random
 
 rupiasEnCofres = [10, 15, 20, 25]
 
-
 def calcular_numero_aleatorio_entre(minimo, maximo):
     return random.randint(minimo, maximo)
-
 
 def abrir_cofre_de_rupias():
     numero_aleatorio = calcular_numero_aleatorio_entre(0, 3)
     rupias_obtenidas = rupiasEnCofres[numero_aleatorio]
     return rupias_obtenidas
 
-
-print(f"Link encontró un tesoro con {abrir_cofre_de_rupias()} rupias")
+print(f"Link encontró un tesoro con {abrir_cofre_de_rupias()} rupias!")
